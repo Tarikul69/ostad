4b13c534009ebe9519b1e2f4a6b1f99a6a731ce0
tarikulabir <tarikulabir931@gmail.com>
2022-12-12 23:44:31 +0600

    stateless-1

--- lib/stateful.dart
+++ lib/stateful.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
 class stateful extends StatefulWidget {
-  const stateful({Key? key}) : super(key: key);
-
+  stateful({Key? key}) : super(key: key);
+  int countNumber=0;
   @override
-  State<stateful> createState() => _statefulState();
+  State<stateful> createState(){
+     return  _statefulState();
+  }
+
 }
 
+
 class _statefulState extends State<stateful> {
+
+  @override
+  void initState(){
+    print("initState called");
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies(){
+    print("didchangeDependencies called");
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('App'),),
+      body: Center(
+        child: Text('Cont ${widget.countNumber}'),
+      ),
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),onPressed: (){
+        setState(() {
+          print("setState");
+          widget.countNumber = widget.countNumber+1;
+        });
+      },),
+    );
   }
 }
